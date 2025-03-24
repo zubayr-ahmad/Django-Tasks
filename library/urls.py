1c136ed723eda0e635210a977ee8fc581bebaecb
Muhammad Zubair <zubayrahmad0308@gmail.com>
2025-03-24 14:46:44 +0500

    implemented ModelViewSets

--- library/urls.py
+++ library/urls.py
@@ -1,16 +1,23 @@
 from django.urls import path
-from .views import (BookListCreateView, 
-                    BookRetrieveUpdateDestroyView,
-                    AuthorListCreateView,
-                    AuthorRetrieveUpdateDestroyView,
-                    GenreListCreateView,
-                    GenreRetrieveUpdateDestroyView
+from rest_framework.routers import DefaultRouter
+from .views import (AuthorViewSet,
+                    BookViewSet,
+                    GenreViewSet
                     )
-urlpatterns = [
-    path('books/', BookListCreateView.as_view(), name='books-list'),
-    path('books/<int:pk>', BookRetrieveUpdateDestroyView.as_view(), name='books-detail'),
-    path("authors/", AuthorListCreateView.as_view(), name="authors-list"),
-    path("authors/<int:pk>/", AuthorRetrieveUpdateDestroyView.as_view(), name="authors-detail"),
-    path('genres/', GenreListCreateView.as_view(), name='genres-list'),
-    path('genres/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genres-detail')
-]
+
+router = DefaultRouter()
+router.register(r'authors', AuthorViewSet, basename='authors')
+router.register(r'books', BookViewSet, basename='books')
+router.register(r'genres', GenreViewSet, basename='genres')
+
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+#     path('books/', BookListCreateView.as_view(), name='books-list'),
+#     path('books/<int:pk>', BookRetrieveUpdateDestroyView.as_view(), name='books-detail'),
+#     path("authors/", AuthorListCreateView.as_view(), name="authors-list"),
+#     path("authors/<int:pk>/", AuthorRetrieveUpdateDestroyView.as_view(), name="authors-detail"),
+#     path('genres/', GenreListCreateView.as_view(), name='genres-list'),
+#     path('genres/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genres-detail')
+# ]
--- library/views.py
+++ library/views.py
@@ -1,28 +1,17 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Book, Author, Genre
 from .serializers import BookSerializer, AuthorSerializer, GenreSerializer
+from rest_framework import viewsets
 
-
-class AuthorListCreateView(ListCreateAPIView):
+class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
-class AuthorRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
 
-class BookListCreateView(ListCreateAPIView):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-class GenreListCreateView(ListCreateAPIView):
-    queryset = Genre.objects.all()
-    serializer_class = GenreSerializer
-
-class GenreRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
